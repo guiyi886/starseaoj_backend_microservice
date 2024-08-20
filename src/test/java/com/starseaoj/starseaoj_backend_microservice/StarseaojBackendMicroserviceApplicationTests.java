a214ed3bb14dc116494f6cb6c980893b7d00ba0f
guiyi <1677852098@qq.com>
2024-08-20 09:48:41 +0800

    init

---
+++ src/test/java/com/starseaoj/starseaoj_backend_microservice/StarseaojBackendMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starseaoj.starseaoj_backend_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarseaojBackendMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
