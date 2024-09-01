7d6d8e4e046bae9eeb23ce0001c02a80e55c6040
guiyi <1677852098@qq.com>
2024-09-01 20:26:09 +0800

    添加feign相关注解注释说明

--- starseaoj-backend-question-service/src/main/java/com/starseaoj/questionservice/StarseaojBackendQuestionServiceApplication.java
+++ starseaoj-backend-question-service/src/main/java/com/starseaoj/questionservice/StarseaojBackendQuestionServiceApplication.java
@@ -14,7 +14,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @ComponentScan("com.starseaoj")
+// 启用服务发现功能，使得应用程序能够注册到服务注册中心，并且能够发现和调用其他注册在服务注册中心的服务。
 @EnableDiscoveryClient
+// 启用 Feign 客户端功能，允许你在 Spring 应用中使用 Feign 来调用其他服务。
+// Feign 是一种声明式的 HTTP 客户端，可以将远程服务调用抽象为接口调用。
+// 如果没有调用其他服务的话可以不添加
 @EnableFeignClients(basePackages = "com.starseaoj.serviceclient")
 public class StarseaojBackendQuestionServiceApplication {
 
