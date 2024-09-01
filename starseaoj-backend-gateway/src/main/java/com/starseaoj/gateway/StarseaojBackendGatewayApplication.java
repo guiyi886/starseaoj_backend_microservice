53aaacb575ba8b1fe748646d2c9c3b320f81997c
guiyi <1677852098@qq.com>
2024-09-01 22:58:24 +0800

    实现网关服务转发

--- starseaoj-backend-gateway/src/main/java/com/starseaoj/gateway/StarseaojBackendGatewayApplication.java
+++ starseaoj-backend-gateway/src/main/java/com/starseaoj/gateway/StarseaojBackendGatewayApplication.java
@@ -2,13 +2,14 @@ package com.starseaoj.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-@SpringBootApplication
+// 排除数据库配置
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @ComponentScan("com.starseaoj")
