a7362769d1411e8f18c4aa7ce15a185a43c10c62
guiyi <1677852098@qq.com>
2024-09-03 12:59:03 +0800

    实现knief4j聚合文档配置以及解决跨路径session问题

--- starseaoj-backend-gateway/src/main/java/com/starseaoj/gateway/StarseaojBackendGatewayApplication.java
+++ starseaoj-backend-gateway/src/main/java/com/starseaoj/gateway/StarseaojBackendGatewayApplication.java
@@ -4,15 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 // 排除数据库配置
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableScheduling
-@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
-@ComponentScan("com.starseaoj")
 @EnableDiscoveryClient
 public class StarseaojBackendGatewayApplication {
 
